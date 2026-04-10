265c5ceda1258ccc54fe7c44329f18da62880aef
alessia263456 <alessiamihaila05@gmail.com>
2026-04-10 20:29:26 +0300

    Lab 6 - 6.8.1

--- src/lab6/advanced/NewIntCalculatorTest.java
+++ src/lab6/advanced/NewIntCalculatorTest.java
@@ -11,45 +11,118 @@ public class NewIntCalculatorTest {
     public void testAddPositives()
     {
         //arrange
-
+        calculator.init();
         //act
-
+        calculator.add(5);
         //assert
+        Assertions.assertEquals(5, calculator.result());
     }
 
     @Test
-    public void testAddNegatives(){}
+    public void testAddNegatives()
+    {
+        //arrange
+        calculator.init();
+        //act
+        calculator.add(-5);
+        //assert
+        Assertions.assertEquals(-5, calculator.result());
+
+    }
 
     @Test
-    public void testSubstractPositives(){}
+    public void testSubstractPositives()
+    {
+        //arrange
+        calculator.init();
+        //act
+        calculator.subtract(5);
+        //assert
+        Assertions.assertEquals(-5, calculator.result());
+    }
 
     @Test
-    public void testSubstractNegatives(){}
+    public void testSubstractNegatives()
+    {
+        //arrange
+        calculator.init();
+        //act
+        calculator.subtract(-5);
+        //assert
+        Assertions.assertEquals(5, calculator.result());
+    }
 
     @Test
-    public void testMultiplyPositives(){}
+    public void testMultiplyPositives()
+    {
+        //arrange
+        calculator.init();
+        calculator.add(1);
+        //act
+        calculator.multiply(5);
+        calculator.multiply(3);
+        //assert
+        Assertions.assertEquals(15, calculator.result());
+    }
 
     @Test
-    public void testMultiplyNegatives(){}
+    public void testMultiplyNegatives()
+    {
+        //arrange
+        calculator.init();
+        calculator.add(1);
+        //act
+        calculator.multiply(-5);
+        calculator.multiply(-3);
+        //assert
+        Assertions.assertEquals(15, calculator.result());
+    }
 
     @Test
     public void testMultiplyBy0()
     {
         //arrange
-
+        calculator.init();
+        calculator.add(1);
         //act
-
+        calculator.multiply(5);
+        calculator.multiply(0);
         //assert
+        Assertions.assertEquals(0, calculator.result());
+
     }
 
     @Test
-    public void testDividePositives(){}
+    public void testDividePositives()
+    {
+        //arrange
+        calculator.init();
+        calculator.add(10);
+        //act
+        calculator.divide(2);
+        //assert
+        Assertions.assertEquals(5, calculator.result());
+
+    }
 
     @Test
-    public void testDivideNegatives(){}
+    public void testDivideNegatives()
+    {
+        //arrange
+        calculator.init();
+        calculator.add(-10);
+        //act
+        calculator.divide(-2);
+        //assert
+        Assertions.assertEquals(5, calculator.result());
+
+    }
 
     @Test
-    public void testDivideBy0(){}
+    public void testDivideBy0()
+    {
+
+    }
 
     @AfterEach
     public void teardown()
