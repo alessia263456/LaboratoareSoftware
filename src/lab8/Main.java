7994de7ebd4080e90d849003f1e0730dadb0f92d
alessia263456 <alessiamihaila05@gmail.com>
2026-05-03 16:49:27 +0300

    Lab 8.5.3

--- src/lab8/Main.java
+++ src/lab8/Main.java
@@ -4,7 +4,6 @@ import java.io.*;
 
 import java.util.Iterator;
 
-import org.apache.poi.*;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.*;
 
@@ -36,7 +35,7 @@ public class Main {
         System.out.println();
     }
 
-    public static void medieNote(XSSFSheet sourceSheet, XSSFWorkbook destinationWorkbook) throws IOException {
+    public static void medieNote1(XSSFSheet sourceSheet, XSSFWorkbook destinationWorkbook) throws IOException {
         XSSFSheet destSheet=destinationWorkbook.createSheet();
         Iterator<Row> rowIterator = sourceSheet.iterator();
         int rowIndex = 0;
@@ -80,6 +79,46 @@ public class Main {
         destinationWorkbook.close();
     }
 
+    public static void medieNote2(XSSFSheet sourceSheet, XSSFWorkbook destinationWorkbook) throws IOException {
+        XSSFSheet destSheet=destinationWorkbook.createSheet();
+        Iterator<Row> rowIterator = sourceSheet.iterator();
+        int rowIndex = 0;
+        while (rowIterator.hasNext()) {
+            Row sourceRow = rowIterator.next();
+            Row destRow = destSheet.createRow(rowIndex);
+
+            int lastCell = sourceRow.getLastCellNum();
+            for (int i = 0; i < lastCell; i++) {
+                Cell sourceCell = sourceRow.getCell(i);
+                Cell destCell = destRow.createCell(i);
+
+                if (sourceCell != null) {
+                    switch (sourceCell.getCellType()) {
+                        case STRING:
+                            destCell.setCellValue(sourceCell.getStringCellValue());
+                            break;
+                        case NUMERIC:
+                            destCell.setCellValue(sourceCell.getNumericCellValue());
+                            break;
+                    }
+                }
+            }
+            if (rowIndex == 0) {
+                destRow.createCell(lastCell).setCellValue("Medie");
+            }
+            else {
+                Cell cell = destRow.createCell(lastCell);
+                String formula = "AVERAGE(D" + (rowIndex + 1) + ":F" + (rowIndex + 1) + ")";
+                cell.setCellFormula(formula);
+            }
+            rowIndex++;
+        }
+        FileOutputStream fos = new FileOutputStream("src/lab8/laborator8_output3.xlsx");
+        destinationWorkbook.write(fos);
+        fos.close();
+        destinationWorkbook.close();
+    }
+
     public static void main(String[] args) throws IOException {
         FileInputStream file1=new FileInputStream(new File("src/lab8/laborator8_input.xlsx"));
         XSSFWorkbook workbook1=new XSSFWorkbook(file1);
@@ -87,9 +126,14 @@ public class Main {
         afisareSheet(sheet1);
 
         XSSFWorkbook workbook2=new XSSFWorkbook();
-        medieNote(sheet1,workbook2);
+        medieNote1(sheet1,workbook2);
         XSSFSheet sheet2=workbook2.getSheetAt(0);
         afisareSheet(sheet2);
+
+        XSSFWorkbook workbook3=new XSSFWorkbook();
+        medieNote2(sheet1,workbook3);
+        XSSFSheet sheet3=workbook3.getSheetAt(0);
+        afisareSheet(sheet3);
 
         file1.close();
         workbook1.close();
