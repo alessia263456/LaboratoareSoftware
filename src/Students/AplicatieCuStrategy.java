1c0f89ba42e1b2dbb57fef45f59e8c6a042537b3
alessia263456 <alessiamihaila05@gmail.com>
2026-05-12 10:55:46 +0300

    Lab 10.5.1 - b) Export Students in fisier txt

--- src/Students/AplicatieCuStrategy.java
+++ src/Students/AplicatieCuStrategy.java
@@ -10,15 +10,20 @@ public class AplicatieCuStrategy {
                 new Student(1024,"Ioan","Mihalcea","ISM141/1", 10),
                 new Student(1026,"Anamaria","Prodan","TI131/1", 8.90),
                 new Student(1029,"Bianca","Popescu","TI131/1,", 10),
-                new Student(1029,"Maria","Pana","TI131/2,", 4.10),
+                new Student(1029,"Maria","Pana","TI131/2", 4.10),
                 new Student(1029,"Gabriela","Mohanu","TI131/2,", 7.33),
-                new Student(1029,"Marius","Nasta","TI131/2,", 3.20),
-                new Student(1029,"Marius","Nasta","TI131/1,", 5.12),
-                new Student(1029,"Andrei","Dobrescu","TI131/2,", 2.22)
+                new Student(1029,"Marius","Nasta","TI131/2", 3.20),
+                new Student(1029,"Marius","Nasta","TI131/1", 5.12),
+                new Student(1029,"Andrei","Dobrescu","TI131/2", 2.22)
         );
         System.out.println("a) Afisare in consola");
         IStudentiExport strategyConsole=new StudentiInConsola();
         Exporter exporter=new Exporter();
         exporter.startExport(strategyConsole, studenti);
+
+        System.out.println("b) Afisare in fisier txt");
+        String fileName = "studentiStrategyText.txt";
+        StudentiInFisierText strategyFisierText = new StudentiInFisierText(fileName);
+        exporter.startExport(strategyFisierText, studenti);
     }
 }
---
+++ src/Students/StudentiInFisierText.java
@@ -0,0 +1,28 @@
+package Students;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class StudentiInFisierText implements IStudentiExport {
+
+    String filename;
+    public StudentiInFisierText(String filename) {
+        this.filename = filename;
+    }
+    @Override
+    public void doExport(List<Student> students) {
+        Path path= Paths.get(filename);
+        try(BufferedWriter writer= Files.newBufferedWriter(path)){
+            for(Student student:students){
+                writer.write(student.toString());
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
